59fcc3673cfb19dd0527cacc3a469a28571a9687
IbnKhaldoun-svg <essalih.idriss.w@gmail.com>
2026-09-02 08:52:17 +0200

    feat(stations): wire session repository in Application

--- app/src/main/java/com/archimede/w2full/W2FullApplication.kt
+++ app/src/main/java/com/archimede/w2full/W2FullApplication.kt
@@ -2,7 +2,12 @@ package com.archimede.w2full
 
 import android.app.Application
 import com.archimede.w2full.data.local.W2FullDatabase
+import com.archimede.w2full.data.mimit.EniStationDistanceService
+import com.archimede.w2full.data.mimit.MimitCsvClient
+import com.archimede.w2full.data.mimit.NearbyStationsRepository
+import com.archimede.w2full.data.mimit.SessionNearbyStationsRepository
 import com.archimede.w2full.data.repository.RefuelingRepository
+import com.archimede.w2full.location.FusedUserLocationProvider
 
 class W2FullApplication : Application() {
     private val database by lazy { W2FullDatabase.getInstance(this) }
@@ -11,6 +16,15 @@ class W2FullApplication : Application() {
         RefuelingRepository(
             vehicleDao = database.vehicleDao(),
             rifornimentoDao = database.rifornimentoDao(),
+        )
+    }
+
+    val nearbyStationsRepository: NearbyStationsRepository by lazy {
+        SessionNearbyStationsRepository(
+            stationSource = MimitCsvClient(),
+            distanceService = EniStationDistanceService(
+                userLocationProvider = FusedUserLocationProvider(this),
+            ),
         )
     }
 }
