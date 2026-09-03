32d6b72b9c9a7586d2ecdd621e18955972eb09f2
IbnKhaldoun-svg <essalih.idriss.w@gmail.com>
2026-09-03 06:45:30 +0200

    chore(android): set M4 RC version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.archimede.w2full"
         minSdk = 26
         targetSdk = 37
-        versionCode = 5
-        versionName = "0.3.2-m4-fuel-setting"
+        versionCode = 6
+        versionName = "0.4.0-rc1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
