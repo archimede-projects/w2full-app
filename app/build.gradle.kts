0a531adf6850c708272603b25a552e95b63e8824
IbnKhaldoun-svg <essalih.idriss.w@gmail.com>
2026-09-04 21:39:07 +0200

    chore(m7.4): bump app version for UX redesign

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.archimede.w2full"
         minSdk = 26
         targetSdk = 37
-        versionCode = 10
-        versionName = "0.5.2-m7.2-rc2"
+        versionCode = 11
+        versionName = "0.5.3-m7.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
